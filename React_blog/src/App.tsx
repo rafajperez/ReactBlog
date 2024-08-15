01a144d96a80f98b64df5bd873e695df26306fe8
rafajperez <rafael_cordas@hotmail.com>
2024-08-14 21:01:22 -0300

    css for navbar

--- React_blog/src/App.tsx
+++ React_blog/src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <div className="container">
         <Outlet />
       </div>
